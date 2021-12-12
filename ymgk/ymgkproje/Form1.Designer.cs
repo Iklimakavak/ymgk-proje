c52a8b85a0071a844ab113fc9906ce93368a0524
Iklimakavak <65605061+Iklimakavak@users.noreply.github.com>
2021-12-13 01:10:13 +0300

    fotoğraf şifreleme uygulaması

---
+++ ymgk/ymgkproje/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(804, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "RESİM EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(804, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ŞİFRELE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(60, 121);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 116);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(255, 121);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(157, 116);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(804, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "ŞİFRELE2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(443, 121);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(157, 116);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(804, 261);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 37);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "DÖNÜŞTÜR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(626, 121);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(157, 116);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
+            this.ClientSize = new System.Drawing.Size(962, 364);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "FOTOĞRAF ŞİFRELEME UYGULAMASI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ ymgk/ymgkproje/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Bitmap GriYap(Bitmap rsim)
+        {
+            for (int i = 0; i < rsim.Height - 1; i++) { 
+                for (int j = 0; j < rsim.Width - 1; j++)
+
+                {
+                    int deger = (rsim.GetPixel(j, i).G + rsim.GetPixel(j, i).B * 0) / 3;
+                    Color renk;
+                    renk = Color.FromArgb(deger, deger, deger);
+                    rsim.SetPixel(j, i, renk);
+                }
+        }
+            return rsim;
+             
+
+                    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+             openFileDialog1.ShowDialog();
+             pictureBox1.ImageLocation = openFileDialog1.FileName;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap resim = new Bitmap(pictureBox1.Image);
+            Bitmap gri = GriYap(resim);
+            pictureBox2.Image = gri;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int xset, yset;
+            var boyut = 5;
+            var goruntu = new Bitmap(pictureBox1.Image);
+            var genislik = goruntu.Width;
+            var yukseklik = goruntu.Height;
+
+            var piksellestirme = new Bitmap(genislik, yukseklik);
+
+            for (var i = 0; i < genislik; i += boyut)
+            {
+                for (var j = 0; j < yukseklik; j += boyut)
+                {
+                    xset = yset = boyut / 2;
+
+                    if(i + xset>= genislik)
+                    {
+                        xset = genislik - i - 1;
+
+                    }
+                    if (j + yset >= genislik)
+                    {
+                        yset = genislik - j - 1;
+
+                    }
+                    var piksel = goruntu.GetPixel(i + xset, j + yset);
+
+                    for (var x = i; x<i+boyut && x < genislik; x++)
+                    {
+                        for (var y= j; y < j + boyut && y < yukseklik; y++)
+                        {
+                            piksellestirme.SetPixel(x, y, piksel);
+                        }
+
+                    }
+                }
+
+            }
+            pictureBox2.Image = piksellestirme;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+         
+        }
+    }
+
+      
+
+    
+    }
+
